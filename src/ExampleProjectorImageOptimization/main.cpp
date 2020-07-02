8bf5cdeaa27d5690fca350c63cb6ff2594448ec3
Oleksii Doronin <oleksii.doronin@gmail.com>
2020-07-02 13:17:13 +0300

    generating ground-true images

--- src/ExampleProjectorImageOptimization/main.cpp
+++ src/ExampleProjectorImageOptimization/main.cpp
@@ -52,18 +52,17 @@ void RenderProjectorImage( const Vec3& position, const std::string& scene_filepa
 }
 
 
-void RenderPerceivedImage( const Real& posX, const Real& posY, DisplayProjectorsShow& show, const std::string& image_filepath )
+void RenderPerceivedImage( const Real& posX, const Real& posY, lfrt::LFRayTracer& renderer, const std::string& image_filepath )
 {
     const Real halfSizeX = display.HalfPhysSize[0];
     const Real halfSizeY = display.HalfPhysSize[1];
-    //std::shared_ptr<const RayGenerator> raygen( new RayGenPinhole( width, height, sceneCornerX, sceneCornerY, sceneDistance ) );
     std::shared_ptr<const RayGenerator> raygen( new RayGenPinhole( width, height, -halfSizeX, -halfSizeY, halfSizeX, halfSizeY, display.ViewerDistance, posX, posY ) );
     std::shared_ptr<SampleGenerator> sampler( new SampleGenUniform(3) );
 
     SampleAccumCV* sampleAccumCV = new SampleAccumCV( width, height );
     std::shared_ptr<SampleAccumulator> sampleAccum( sampleAccumCV );
 
-    show.Render( *raygen, *sampler, *sampleAccum );
+    renderer.Render( *raygen, *sampler, *sampleAccum );
 
     cv::Mat result;
     sampleAccumCV->SaveToImage( result );
@@ -100,8 +99,9 @@ int main( int argc, char** argv )
     std::cout << std::endl;
     std::cout << "Initializing basic parameters complete." << std::endl;
     std::cout << "What would you like to do?" << std::endl;
-    std::cout << "1 - generate projector images" << std::endl;
-    std::cout << "2 - generate perceived images" << std::endl;
+    std::cout << "1 - generate ground-true images" << std::endl;
+    std::cout << "2 - generate projector images" << std::endl;
+    std::cout << "3 - generate perceived images (requires step 2)" << std::endl;
 
     Int choice = -1;
     std::cin >> choice;
@@ -109,17 +109,28 @@ int main( int argc, char** argv )
     switch ( choice )
     {
     case 1: {
-        std::system( "mkdir ProjectorImages" );
-        for ( Int i = 0; i < projectorPositions.size(); ++i )
+        std::system( "mkdir GroundTrueImages" );
+        LFRayTracer* raytracer = LFRayTracerPBRTInstance();
+        for ( Int viewInd = 0; viewInd < numViewers; ++viewInd )
         {
-            const std::string image_filepath = (ss() << "ProjectorImages" << "/" << std::setfill('0') << std::setw(4) << i << ".exr").str();
-            RenderProjectorImage( projectorPositions[i], argv[1], image_filepath );
+            const Vec3 viewerPos = viewerStart + Real(viewInd) * viewerStep;
+            const std::string image_filepath = (ss() << "GroundTrueImages" << "/" << std::setfill('0') << std::setw(4) << viewInd << ".exr").str();
+            raytracer->LoadScene( argv[1] );
+            RenderPerceivedImage( viewerPos[0], viewerPos[1], *raytracer, image_filepath );
         }
         } break;
     case 2: {
-        std::system( "mkdir PerceivedImages" );
+        std::system( "mkdir ProjectorImages_0000" );
+        for ( Int i = 0; i < projectorPositions.size(); ++i )
+        {
+            const std::string image_filepath = (ss() << "ProjectorImages_0000" << "/" << std::setfill('0') << std::setw(4) << i << ".exr").str();
+            RenderProjectorImage( projectorPositions[i], argv[1], image_filepath );
+        }
+        } break;
+    case 3: {
+        std::system( "mkdir PerceivedImages_0000" );
         DisplayProjectorsShow show( &display );
-        if ( !show.LoadScene( "./ProjectorImages" ) )
+        if ( !show.LoadScene( "ProjectorImages_0000" ) )
         {
             std::cout << "Could not load projector images! Terminate!" << std::endl;
             return 1;
@@ -127,7 +138,7 @@ int main( int argc, char** argv )
         for ( Int viewInd = 0; viewInd < numViewers; ++viewInd )
         {
             const Vec3 viewerPos = viewerStart + Real(viewInd) * viewerStep;
-            const std::string image_filepath = (ss() << "PerceivedImages" << "/" << std::setfill('0') << std::setw(4) << viewInd << ".exr").str();
+            const std::string image_filepath = (ss() << "PerceivedImages_0000" << "/" << std::setfill('0') << std::setw(4) << viewInd << ".exr").str();
             RenderPerceivedImage( viewerPos[0], viewerPos[1], show, image_filepath );
         }
         } break;
