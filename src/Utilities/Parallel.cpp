85fed67fd19c6dfd94ca5638cb24fdb84c2a0e49
Oleksii Doronin <oleksii.doronin@tuni.fi>
2020-09-27 13:09:30 +0300

    moved implementation of Parallel to cpp file

---
+++ src/Utilities/Parallel.cpp
@@ -0,0 +1,16 @@
+#include "Parallel.h"
+
+
+void CVParallel2D( const Int& width, const Int& height, std::function<void(const Int x, const Int y)> functor )
+{
+	cv::parallel_for_(
+		cv::Range( 0, width*height ),
+		cv::ParallelLoopBodyLambdaWrapper( [&]( const cv::Range& range )
+			{
+				for ( int pixelInd = range.start; pixelInd < range.end; ++pixelInd )
+				{
+					functor( pixelInd % width, pixelInd / width );
+				}
+			} )
+	);
+}
--- src/Utilities/Parallel.h
+++ src/Utilities/Parallel.h
@@ -3,18 +3,9 @@
 
 #include "BaseTypes.h"
 
-void CVParallel2D( const Int& width, const Int& height, std::function<void(const Int x, const Int y)> functor )
-{
-	cv::parallel_for_(
-		cv::Range( 0, width*height ),
-		cv::ParallelLoopBodyLambdaWrapper( [&]( const cv::Range& range )
-			{
-				for ( int pixelInd = range.start; pixelInd < range.end; ++pixelInd )
-				{
-					functor( pixelInd % width, pixelInd / width );
-				}
-			} )
-	);
-}
+void CVParallel2D(
+	const Int& width, const Int& height,
+	std::function<void(const Int x, const Int y)> functor );
+
 
 #endif // UTILITIES_PARALLEL_H
