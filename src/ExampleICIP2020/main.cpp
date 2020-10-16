5cc3834d6b470c88c348bcb0c1aa5b99d18da7ef
Oleksii Doronin <oleksii.doronin@tuni.fi>
2020-10-16 13:21:15 +0300

    render all test cases

--- src/ExampleICIP2020/main.cpp
+++ src/ExampleICIP2020/main.cpp
@@ -12,8 +12,9 @@
 #include "ImageAnalysis.h"
 #include "SampleAccumCV.h"
 #include "SampleGenUniform.h"
+#include "SampleGenDisk.h"
 
-#include "RayGenPinhole.h"
+#include "RayGenFocusEye.h"
 
 
 using namespace lfrt;
@@ -32,103 +33,56 @@ const Real sceneCornerX = 90;
 const Real sceneCornerY = 90;
 const Real sceneDistance = 300;
 
+// Eye model parameters.
+const Real RetinaToPupilDist = 17;
+
 // Display model itself.
 DisplayLenslet display;
 
 
-struct TestCase
+struct DisplayRenderCase
 {
     std::string Name;
     DisplayLensletCapture::Sampling SamplingType;
-    Int NumSamples;
+    Int NumSecondarySamples;
+};
+
+struct PerceivedRenderCase
+{
+    std::string Name;
+    Int NumSecondarySamples;
+    Real ObjectInFocusDistance;
 };
 
 const std::string output_folder = "output";
-const Int NumPerceivedSamples = 5;
+//const Int NumPrimarySamples = 5;
+//const Int NumSecondarySamples = 8;
+const Int NumPrimarySamples = 1;
+const Int NumSecondarySamples = 5;
 
 
 // Provided ray tracer must contain already-loaded scene.
-void RenderGroundTruth( const LFRayTracer* raytracer, cv::Mat& gtimage )
+void RenderPerceivedImage( const LFRayTracer* raytracer, cv::Mat& perceived, const PerceivedRenderCase& testCase )
 {
 
-    std::cout << "Ground-true image render started." << std::endl;
-    std::shared_ptr<const RayGenerator> raygen( new RayGenPinhole( width, height, sceneCornerX, sceneCornerY, sceneDistance ) );
-    std::shared_ptr<SampleGenerator> sampleGen( new SampleGenUniform( NumPerceivedSamples ) );
+    std::cout << "Perceived image render started." << std::endl;
+    auto raygenFocus = new RayGenFocusEye();
+    raygenFocus->Width  = width;
+    raygenFocus->Height = height;
+    raygenFocus->RetinaPlaneZ = -RetinaToPupilDist;
+    raygenFocus->InFocusPlaneZ = testCase.ObjectInFocusDistance;
+    const Real retCoef = -RetinaToPupilDist / sceneDistance;
+    raygenFocus->RetinaStart = lfrt::VEC2({ -sceneCornerX*retCoef, -sceneCornerY*retCoef });
+    raygenFocus->RetinaEnd   = lfrt::VEC2({  sceneCornerX*retCoef,  sceneCornerY*retCoef });
+    std::shared_ptr<const RayGenerator> raygen( raygenFocus );
+    std::shared_ptr<SampleGenerator> sampleGen( new SampleGenDisk( NumPrimarySamples, testCase.NumSecondarySamples ) );
     SampleAccumCV* sampleAccumCV = new SampleAccumCV( width, height );
     std::shared_ptr<SampleAccumulator> sampleAccum( sampleAccumCV );
     raytracer->Render( *raygen, *sampleGen, *sampleAccum );
-    sampleAccumCV->SaveToImage( gtimage );
-    std::cout << "Ground-true image render ended." << std::endl;
+    sampleAccumCV->SaveToImage( perceived );
+    std::cout << "Perceived image render ended." << std::endl;
 }
 
-
-// Provided ray tracer must contain already-loaded scene.
-void RunTestCase( const TestCase& testCase, const LFRayTracer* raytracer, const cv::Mat& gtimage )
-{
-    // Define case-related images images.
-    cv::Mat displayimage;
-    cv::Mat perceivedimage;
-
-    // Create case-related folder.
-    const std::string casedir = output_folder + "/" + testCase.Name;
-    if ( !std::filesystem::exists( casedir ) )
-    {
-        if ( !std::filesystem::create_directory( casedir ) )
-        {
-            std::cout << "Error: Cannot create directory: " << casedir << std::endl;
-            return;
-        }
-    }
-
-    // Render display image.
-    std::cout << "Display image render started." << std::endl;
-    {
-        const Int width = display.ResolutionLCD[0];
-        const Int height = display.ResolutionLCD[1];
-        DisplayLensletCapture* displayRaygen = new DisplayLensletCapture( &display, testCase.SamplingType );
-        std::shared_ptr<const RayGenerator> raygen( displayRaygen );
-        std::shared_ptr<SampleGenerator> sampleGen( new SampleGenUniform(testCase.NumSamples) );
-        SampleAccumCV* sampleAccumCV = new SampleAccumCV( width, height );
-        std::shared_ptr<SampleAccumulator> sampleAccum( sampleAccumCV );
-        raytracer->Render( *raygen, *sampleGen, *sampleAccum );
-        sampleAccumCV->SaveToImage( displayimage );
-    }
-    std::cout << "Display image render ended." << std::endl;
-    cv::imwrite( casedir+"/displayimage.exr", displayimage );
-
-    // Render perceived image.
-    std::cout << "Display simulation started." << std::endl;
-    {
-        const Int numLensletsX = std::round( display.SizeLCD[0] / display.LensletOrientation()(0,0) );
-        const Int numLensletsY = std::round( display.SizeLCD[1] / display.LensletOrientation()(1,1) );
-        DisplayLensletShow renderer( &display );
-        renderer.DisplayImage = displayimage;
-        std::shared_ptr<const RayGenerator> raygen( new RayGenPinhole( width, height, sceneCornerX, sceneCornerY, sceneDistance ) );
-        std::shared_ptr<SampleGenerator> sampler( new SampleGenUniform( NumPerceivedSamples ) );
-        SampleAccumCV* sampleAccumCV = new SampleAccumCV( width, height );
-        std::shared_ptr<SampleAccumulator> sampleAccum( sampleAccumCV );
-        renderer.Render( *raygen, *sampler, *sampleAccum );
-        sampleAccumCV->SaveToImage( perceivedimage );
-    }
-    std::cout << "Display simulation ended." << std::endl;
-    cv::imwrite( casedir+"/simulated.exr", perceivedimage );
-
-    // Compare perceived image vs ground truth.
-    const cv::Scalar psnr = ImageValuePSNR( perceivedimage, gtimage );
-    std::cout << "PSNR: " << psnr[0] << " " << psnr[1] << " " << psnr[2] << std::endl;
-    const cv::Scalar ssim = ImageValueMSSIM( perceivedimage, gtimage );
-    std::cout << "SSIM: " << ssim[0] << " " << ssim[1] << " " << ssim[2] << std::endl;
-    // Write statistics to file.
-    std::fstream file( casedir+"/statistics.txt" , std::fstream::out );
-    if ( !file.is_open() )
-    {
-        std::cout << "Error: Cannot create text file to write statistics." << std::endl;
-        return;
-    }
-    file << "PSNR: " << psnr[0] << " " << psnr[1] << " " << psnr[2] << std::endl;
-    file << "MS-SSIM: " << ssim[0] << " " << ssim[1] << " " << ssim[2] << std::endl;
-    file.close();
-}
 
 
 int main(int argc, char** argv)
@@ -165,11 +119,20 @@ int main(int argc, char** argv)
         }
     }
 
-    // Define test cases parameters.
-    const std::vector<TestCase> testCases({
-        TestCase({ "LC", DisplayLensletCapture::Sampling::LensletCenter, 1 }),
-        TestCase({ "PC", DisplayLensletCapture::Sampling::PupilCenter, 1 }),
-        TestCase({ "LA", DisplayLensletCapture::Sampling::LensletAverage, 5 })
+    // Define display test cases parameters.
+    const std::vector<DisplayRenderCase> rtCases({
+        DisplayRenderCase({ "LC", DisplayLensletCapture::Sampling::LensletCenter, 1 }),
+        DisplayRenderCase({ "PC", DisplayLensletCapture::Sampling::PupilCenter, 1 }),
+        DisplayRenderCase({ "LA", DisplayLensletCapture::Sampling::LensletAverage, 5 })
+        }
+    );
+
+    // Define peceived test cases parameters.
+    const std::vector<PerceivedRenderCase> camCases({
+        PerceivedRenderCase({ "Pin", 1, 0 }),
+        PerceivedRenderCase({ "Near", NumSecondarySamples, 300 }),
+        PerceivedRenderCase({ "Cent", NumSecondarySamples, 400 }),
+        PerceivedRenderCase({ "Far", NumSecondarySamples, 600 }),
         }
     );
 
@@ -185,14 +148,85 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    cv::Mat gt_image;
-    RenderGroundTruth( raytracer, gt_image );
-    cv::imwrite( output_folder+"/gt.exr", gt_image );
+    const Int numCamCases = camCases.size();
+    const Int numRtCases = rtCases.size();
 
-    for ( auto caseiter = testCases.begin(); caseiter != testCases.end(); ++caseiter )
+    // Render ground true images.
+    std::vector<cv::Mat> gtimages( numCamCases );
+    for ( Int camCaseInd = 0; camCaseInd < numCamCases; ++camCaseInd )
     {
-        RunTestCase( *caseiter, raytracer, gt_image );
+        const auto& camCase = camCases[camCaseInd];
+        cv::Mat& gt_image = gtimages[camCaseInd];
+        RenderPerceivedImage( raytracer, gt_image, camCase );
+        const std::string filename = output_folder + "/gt_" + camCase.Name + ".exr";
+        cv::imwrite( filename, gt_image );
     }
+
+    // Render display images, simulate display, and compare to GT.
+    std::vector<cv::Scalar> msevals( numCamCases*numRtCases );
+    std::vector<cv::Scalar> psnrvals( numCamCases*numRtCases );
+    std::vector<cv::Scalar> msssimvals( numCamCases*numRtCases );
+    for ( Int rtCaseInd = 0; rtCaseInd < numRtCases; ++rtCaseInd )
+    {
+        const auto& rtCase = rtCases[rtCaseInd];
+        // Render display image.
+        cv::Mat displayimage;
+        std::cout << "Display image render started." << std::endl;
+        {
+            const Int width = display.ResolutionLCD[0];
+            const Int height = display.ResolutionLCD[1];
+            DisplayLensletCapture* displayRaygen = new DisplayLensletCapture( &display, rtCase.SamplingType );
+            std::shared_ptr<const RayGenerator> raygen( displayRaygen );
+            std::shared_ptr<SampleGenerator> sampleGen( new SampleGenUniform( NumPrimarySamples, rtCase.NumSecondarySamples ) );
+            SampleAccumCV* sampleAccumCV = new SampleAccumCV( width, height );
+            std::shared_ptr<SampleAccumulator> sampleAccum( sampleAccumCV );
+            raytracer->Render( *raygen, *sampleGen, *sampleAccum );
+            sampleAccumCV->SaveToImage( displayimage );
+            const std::string filename = output_folder + "/display_" + rtCase.Name + ".exr";
+            cv::imwrite( filename, displayimage );
+        }
+        std::cout << "Display image render ended." << std::endl;
+        // Create display simulation.
+        DisplayLensletShow renderer( &display );
+        renderer.DisplayImage = displayimage;
+        // Render display simulation and compare to GT.
+        for ( Int camCaseInd = 0; camCaseInd < numCamCases; ++camCaseInd )
+        {
+            const auto& camCase = camCases[camCaseInd];
+            cv::Mat perceived;
+            RenderPerceivedImage( &renderer, perceived, camCase );
+            const std::string filename = output_folder + "/sim_" + rtCase.Name + "_" + camCase.Name + ".exr";
+            cv::imwrite( filename, perceived );
+            const auto& gtimage = gtimages[camCaseInd];
+            const Int statInd = rtCaseInd*numCamCases + camCaseInd;
+            msevals[statInd] = ImageValueMSE( perceived, gtimage );
+            psnrvals[statInd] = ImageValuePSNR( perceived, gtimage );
+            msssimvals[statInd] = ImageValueMSSIM( perceived, gtimage );
+        }
+    }
+
+    // Save statistics to file.
+    std::fstream msefile( output_folder + "/mse.txt", std::fstream::out );
+    std::fstream psnrfile( output_folder + "/psnr.txt", std::fstream::out );
+    std::fstream msssimfile( output_folder + "/msssim.txt", std::fstream::out );
+    for ( Int rtCaseInd = 0; rtCaseInd < numRtCases; ++rtCaseInd )
+    {
+        const auto& rtCase = rtCases[rtCaseInd];
+        for ( Int camCaseInd = 0; camCaseInd < numCamCases; ++camCaseInd )
+        {
+            const auto& camCase = camCases[camCaseInd];
+            const Int statInd = rtCaseInd*numCamCases + camCaseInd;
+            const auto& mse = msevals[statInd];
+            const auto& psnr = psnrvals[statInd];
+            const auto& msssim = msssimvals[statInd];
+            msefile << (rtCase.Name+"_"+camCase.Name) << " " << mse[0] << " " << mse[1] << " " << mse[2] << std::endl;
+            psnrfile << (rtCase.Name+"_"+camCase.Name) << " " << psnr[0] << " " << psnr[1] << " " << psnr[2] << std::endl;
+            msssimfile << (rtCase.Name+"_"+camCase.Name) << " " << msssim[0] << " " << msssim[1] << " " << msssim[2] << std::endl;
+        }
+    }
+    msefile.close();
+    psnrfile.close();
+    msssimfile.close();
 
     LFRayTRacerPBRTRelease();
 
